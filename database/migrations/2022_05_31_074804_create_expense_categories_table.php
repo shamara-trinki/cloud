c8cf65862aa9ce75cc52cbaae9948b5d80ba98bc
Shamara-Trinki <shamara.trinki@gmail.com>
2025-07-14 16:13:01 +0530

    Add server database field

--- database/migrations/2022_05_31_074804_create_expense_categories_table.php
+++ database/migrations/2022_05_31_074804_create_expense_categories_table.php
@@ -15,8 +15,21 @@ class CreateExpenseCategoriesTable extends Migration
     {
         Schema::create('expense_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('server_id ');
             $table->string('name');
-            $table->string('color', 20);
+            $table->string('server_type');
+            $table->string('ip_address');
+            $table->string('ram');
+            $table->string('storage');
+            $table->integer('rdb_port');
+            $table->string('ac_backup');
+            $table->date('paid_date');
+            $table->string('renewal_amt')->nullable();
+            $table->date('renewal_date')->nullable();
+            $table->text('email')->nullable();
+            $table->string('email_password')->nullable();
+            $table->string('billing_cycle')->nullable();
+             $table->string('color', 20);
             $table->text('description')->nullable();
             $table->timestamps();
         });
